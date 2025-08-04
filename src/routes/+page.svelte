5e7ff646b2918888135aabe3b1705dc14bc2b387
Josh Sawyer <jawshs08@gmail.com>
2025-08-04 03:11:52 -0400

    create card skeleton for projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,51 @@
-<div class="bg-gradient-to-b from-yellow-100 to-orange-500 h-screen">
-  <div class="h-screen flex flex-col justify-center items-center">
-    <h1 class="text-xl">Hi, my name is</h1>
-    <h2 class="text-5xl font-bold">Josh Sawyer</h2>
-    <p class="text-lg text-justify">
-      I'm a third year Computer Science major at the Univerity of Central
-      Florida!
-    </p>
-    <div>
-      <a class="" href="https://www.linkedin.com/in/josh-sawyer-b765ab255/" target="_blank">
-        <!-- <img src=""> -->
-        <!-- Use this ^^^ -->
-        LinkedIn
-      </a>
-      <a class="" href="https://github.com/buhgowsh" target="_blank">
-        <!-- <img src=""> -->
-        <!-- Use this ^^^ -->
-        GitHub
-      </a>
-      <a class="" href="/resume.pdf" target="_blank">
-        <!-- <img src=""> -->
-        <!-- Use this ^^^ -->
-        Resume
-      </a>
+<div id="wrapper">
+  <!-- Introduction and links to socials as first screen -->
+  <div class="bg-gradient-to-b from-yellow-100 to-orange-500 h-screen">
+
+    <!-- Each tag goes on its own line -->
+    <div class="h-screen flex flex-col justify-center items-center">
+      <h1 class="text-xl">Hi, my name is</h1>
+      <h2 class="text-5xl font-bold">Josh Sawyer</h2>
+      <p class="text-lg text-justify">
+        I'm a third year Computer Science major at the Univerity of Central
+        Florida!
+      </p>
+
+      <!-- Links to socials combined to fit in a single line -->
+      <div>
+        <a class="" href="https://www.linkedin.com/in/josh-sawyer-b765ab255/" target="_blank">
+          <!-- <img src=""> -->
+          <!-- Use this ^^^ -->
+          LinkedIn
+        </a>
+        <a class="" href="https://github.com/buhgowsh" target="_blank">
+          <!-- <img src=""> -->
+          <!-- Use this ^^^ -->
+          GitHub
+        </a>
+        <a class="" href="/resume.pdf" target="_blank">
+          <!-- <img src=""> -->
+          <!-- Use this ^^^ -->
+          Resume
+        </a>
+      </div>
     </div>
   </div>
 
+  <!-- Start of the experience section, filled with cards -->
   <div
-    class="h-screen flex justify-center items-center bg-gradient-to-b from-orange-500 to-orange-900"
+    class="h-screen flex flex-col justify-center items-center bg-gradient-to-b from-orange-500 to-amber-400"
   >
     <p>here's where the cards are gonna go</p>
+    <div class="max-w-sm rounded overflow-hidden hover:shadow-lg shadow-white bg-gray-100">
+      <!-- <img src=""> use this once you have a pic for the card -->
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Something</div>
+        <p class="text-gray-700 text-base">description of something</p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#fax</span>
+      </div>
+    </div>
   </div>
 </div>

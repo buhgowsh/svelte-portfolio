01b8037e00c0e5a73932345d6b061cd4015ade00
Josh Sawyer <jawshs08@gmail.com>
2025-08-24 14:07:38 -0400

    clean up cards some more

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
   <div class="bg-gradient-to-b from-yellow-100 to-orange-500 h-screen">
     <!-- Each tag goes on its own line -->
     <div class="h-screen flex flex-col justify-center items-center">
-      <h1 class="text-xl flex flex -row">Hi, my name is</h1>
-      <h2 class="text-5xl font-bold">Josh Sawyer</h2>
-      <p class="text-lg text-justify">
+      <h1 class="text-4xl flex flex -row">Hi, my name is</h1>
+      <h2 class="text-6xl md:text-7xl font-bold">Josh Sawyer</h2>
+      <p class="text-xl md:text-2xl text-justify">
         I'm a third year Computer Science major at the Univerity of Central
         Florida!
       </p>
@@ -18,7 +18,7 @@
           target="_blank"
         >
           <img
-            class="h-15 w-15 object-contain rounded"
+            class="h-15 md:h-20 w-15 md:w-20 rounded"
             src="/icons8-linkedin.svg"
             alt="LinkedIn button"
             draggable="false"
@@ -26,12 +26,12 @@
         </a>
 
         <a
-          class="hover:motion-preset-seesaw hover:motion-duration-1000 px-5"
+          class="hover:motion-rotate-loop-10 hover:motion-duration-1000 px-5"
           href="https://github.com/buhgowsh"
           target="_blank"
         >
           <img
-            class="h-15 w-15 rounded"
+            class="h-15 md:h-20 w-15 md:w-20 rounded"
             src="/icons8-github.svg"
             alt="GitHub button"
             draggable="false"
@@ -39,12 +39,12 @@
         </a>
 
         <a
-          class="hover:motion-rotate-loop-10 hover:motion-duration-1000 px-5"
+          class="hover:motion-rotate-loop-10 hover:motion-duration-1000 px-4"
           href="/resume.pdf"
           target="_blank"
         >
           <img
-            class="h-15 w-15"
+            class="h-15 md:h-20 w-15 md:w-20"
             src="/icons8-resume.png"
             alt="Resume button"
             draggable="false"
@@ -56,33 +56,62 @@
 
   <!-- Should add an Experience section with the miller internship, HSPT, and hackathons here -->
 
-  <!-- Start of the experience section, filled with cards -->
-  <div
-    class="h-screen flex flex-col justify-center items-center bg-gradient-to-b from-orange-500 to-amber-400"
+  <!-- This is a skeleton for extra cards in the future
+  add a place to route
+  <a
+    href="/"
+    class="mx-10 max-w-sm rounded overflow-hidden bg-gray-100 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:shadow-lg shadow-white motion-reduce:transition-none motionreduce:hover:transform-none"
   >
-    <h1 class="text-5xl text-bold py-8">Projects</h1>
+    <img class="w-full h-auto object-cover" src="" alt=""> use this once you have a pic for the card
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl md:text-3xl mb-2">Something</div>
+      <p class="text-gray-700 text-base">description of something</p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      Each tag goes here
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
+        >#fax</span
+      >
+    </div>
+  </a> -->
+
+  <!-- Start of the projects section, filled with cards -->
+  <div
+    class="flex flex-col justify-center items-center bg-gradient-to-b from-orange-500 to-amber-400"
+  >
+    <h1 class="text-4xl md:text-6xl text-bold py-8">Projects</h1>
     <div id="cards" class="flex flex-col">
       <div id="cards first row" class="flex flex-row mx-auto">
         <a
           href="/gnarpnotes"
           class="mx-10 max-w-sm rounded overflow-hidden bg-gray-100 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:shadow-lg shadow-white motion-reduce:transition-none motionreduce:hover:transform-none"
         >
-          <img src="/gnarpnotes.png" alt="Gnarpy face" />
+          <img
+            class="w-full h-auto object-cover"
+            src="/gnarpnotes.png"
+            alt="Gnarpy face"
+          />
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Gnarp Notes</div>
-            <p class="text-gray-700 text-base">
-              AI learning assistant using flash cards!
+            <div class="font-bold text-xl md:text-3xl mb-2">Gnarp Notes</div>
+            <p class="text-lg md:text-xl text-gray-700 text-base">
+              AI learning assistant using flash cards, and downloadable as a
+              PowerPoint file!
             </p>
           </div>
           <div class="px-6 pt-4 pb-2">
             <!-- Each tag goes here -->
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >GeminiAPI</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >React</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
+              >Hackathon</span
             >
           </div>
         </a>
@@ -91,28 +120,43 @@
           href="/nova"
           class="mx-10 max-w-sm rounded overflow-hidden bg-gray-100 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:shadow-lg shadow-white motion-reduce:transition-none motionreduce:hover:transform-none"
         >
-          <img src="/nova.png" alt="Title Card for NOVA AI" />
+          <img
+            class="w-full h-auto object-cover"
+            src="/nova.png"
+            alt="Title Card for NOVA AI"
+          />
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">N.O.V.A. AI</div>
-            <p class="text-gray-700 text-base">AI powered attention tracker!</p>
+            <div class="font-bold text-xl md:text-3xl mb-2">N.O.V.A. AI</div>
+            <p class="text-lg md:text-xl text-gray-700 text-base">
+              AI powered attention tracker!
+            </p>
+            <div
+              class="text-lg md:text-xl -mx-6 mb-3 text-center py-1 px-1 my-1 tracking-wider bg-clip-border bg-amber-300"
+            >
+              Won Best Design @ Hack@USF 2025!
+            </div>
           </div>
           <div class="px-6 pt-4 pb-2">
             <!-- Each tag goes here -->
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >OpenAI API</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >React</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >Python</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >Matplotlib</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
+              >Hackathon</span
             >
           </div>
         </a>
@@ -121,107 +165,114 @@
           href="/fishnet"
           class="mx-10 max-w-sm rounded overflow-hidden bg-gray-100 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:shadow-lg shadow-white motion-reduce:transition-none motionreduce:hover:transform-none"
         >
-          <img src="/fishnet.png" alt="Globe view of FishNet discovery page" />
+          <img
+            class="w-full h-auto object-cover"
+            src="/fishnet.png"
+            alt="Globe view of FishNet discovery page"
+          />
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">FishNet</div>
-            <p class="text-gray-700 text-base">
-              Platform for sharing pictures of aquatic life that can be seen on a rendered globe!
+            <div class="font-bold text-xl md:text-3xl mb-2">FishNet</div>
+            <p class="text-lg md:text-xl text-gray-700 text-base">
+              Platform for sharing pictured of aquatic life! Built upon The
+              Coral Collection.
             </p>
           </div>
           <div class="px-6 pt-4 pb-2">
             <!-- Each tag goes here -->
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >React</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >MongoDB</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >RESTful APIs</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >Mailgun</span
+            >
+            <span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
+              >Mapbox GL</span
             >
           </div>
         </a>
       </div>
 
       <div id="cards second row" class="flex flex-row mx-auto my-10">
-        <!-- Example card -->
         <a
           href="/coralcollection"
           class="mx-10 max-w-sm rounded overflow-hidden bg-gray-100 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:shadow-lg shadow-white motion-reduce:transition-none motionreduce:hover:transform-none"
         >
-          <img src="/coralcollection.png" alt="" />
+          <img
+            class="w-full h-auto object-cover"
+            src="/coralcollection.png"
+            alt=""
+          />
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coral Collection</div>
-            <p class="text-gray-700 text-base">
+            <div class="font-bold text-xl md:text-3xl mb-2">
+              The Coral Collection
+            </div>
+            <p class="text-lg md:text-xl text-gray-700 text-base">
               Simple ocean-based contact manager.
             </p>
           </div>
           <div class="px-6 pt-4 pb-2">
             <!-- Each tag goes here -->
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >HTML</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >PHP</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >RESTful APIs</span
             >
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
               >MySQL</span
             >
           </div>
         </a>
 
-        <!-- add a place to route -->
         <a
-          href="/"
+          href="/portfolio"
           class="mx-10 max-w-sm rounded overflow-hidden bg-gray-100 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:shadow-lg shadow-white motion-reduce:transition-none motionreduce:hover:transform-none"
         >
-          <!-- <img src="" alt=""> use this once you have a pic for the card -->
+          <img
+            class="w-full h-auto object-cover"
+            src="/portfolio.png"
+            alt="front page of portfolio"
+          />
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Something</div>
-            <p class="text-gray-700 text-base">description of something</p>
+            <div class="font-bold text-xl md:text-3xl mb-2">Portfolio Site</div>
+            <p class="text-lg md:text-xl text-gray-700 text-base">
+              The portfolio itself.
+            </p>
           </div>
           <div class="px-6 pt-4 pb-2">
             <!-- Each tag goes here -->
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-              >#fax</span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
+              >Svelte</span
             >
-          </div>
-        </a>
-
-        <!-- add a place to route -->
-        <a
-          href="/"
-          class="mx-10 max-w-sm rounded overflow-hidden bg-gray-100 transition delay-150 duration-300 ease-in-out hover:scale-110 hover:shadow-lg shadow-white motion-reduce:transition-none motionreduce:hover:transform-none"
-        >
-          <!-- <img src="" alt=""> use this once you have a pic for the card -->
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Something</div>
-            <p class="text-gray-700 text-base">description of something</p>
-          </div>
-          <div class="px-6 pt-4 pb-2">
-            <!-- Each tag goes here -->
             <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-              >#fax</span
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm md:text-lg font-semibold text-gray-700 mr-2 mb-2"
+              >TailwindCSS</span
             >
           </div>
         </a>
       </div>
     </div>
+    <footer class="relative bottom-1 text-center">
+      Made with ❤️ from Josh Sawyer - Icons from Icons8
+    </footer>
   </div>
 </div>

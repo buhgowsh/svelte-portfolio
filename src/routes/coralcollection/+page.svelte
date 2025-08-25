bb79466c9ad5ac8a3d1ec76964fe9756c81ce4fa
Josh Sawyer <jawshs08@gmail.com>
2025-08-25 19:46:25 -0400

    add backgrounds to some project pages

--- src/routes/coralcollection/+page.svelte
+++ src/routes/coralcollection/+page.svelte
@@ -1,12 +1,15 @@
-<h1>The Coral Collection</h1>
+<div
+  class="flex flex-col items-center h-screen bg-cover bg-[url(/coral-background.jpg)]"
+>
+  <h1 class="text-white font-bold py-20">The Coral Collection</h1>
 
-<p>
-    The Coral Collection was a very simple contact manager that had an aquatic theme to it. 
-    The MySQL database was extremely simple, with only two tables: one for registered users
-    and one for contacts added by users. RESTful calls to the database were done using PHP.
-    I was in charge of the database, and made some of the APIs.
-</p>
+  <p class="text-white font-bold">
+    The Coral Collection was a very simple contact manager that had an aquatic
+    theme to it. The MySQL database was extremely simple, with only two tables:
+    one for registered users and one for contacts added by users. RESTful calls
+    to the database were done using PHP. I was in charge of the database, and
+    made some of the APIs.
+  </p>
 
-<p>stuff to do:
-    add pics from the powerpoint
-</p>
+  <p>stuff to do: add pics from the powerpoint</p>
+</div>
--- src/routes/gnarpnotes/+page.svelte
+++ src/routes/gnarpnotes/+page.svelte
@@ -1,16 +1,20 @@
-<body class="bg-slate-900">
-    <div class="min-h-screen w-full bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] p-2 sm:p-4 flex items-center justify-center text-white">
-        <p>Gnarp Notes</p>
+<main class="bg-slate-900">
+  <div
+    class="min-h-screen w-full bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] p-2 sm:p-4 flex flex-col items-center text-white"
+  >
+    <h1 class="py-20">Gnarp Notes</h1>
 
-        <p>
-            Gnarp Notes is a study tool that takes in PDF files from users and converts them into flash cards
-            using the Google GeminiAPI, with an option to download the generated flash cards as a PowerPoint
-            to use to study later. This was a solo hackathon project that I created during Gemiknights, an AI focused
-            hackathon hosted by Knight Hacks at UCF. The overall design of the project went through a couple
-            iterations, but the core theme of Gnarpy the alien cat stayed the same. Converting the flash cards 
-            into a downloadable PowerPoint was the most challenging part of the project, since finding and 
-            implementing the library I used (pptxgenjs) took most of the time I had.
-        </p>
-
-    </div>
-</body>
+    <p>
+      Gnarp Notes is a study tool that takes in PDF files from users and
+      converts them into flash cards using the Google GeminiAPI, with an option
+      to download the generated flash cards as a PowerPoint to use to study
+      later. This was a solo hackathon project that I created during
+      Gemiknights, an AI focused hackathon hosted by Knight Hacks at UCF. The
+      overall design of the project went through a couple iterations, but the
+      core theme of Gnarpy the alien cat stayed the same. Converting the flash
+      cards into a downloadable PowerPoint was the most challenging part of the
+      project, since finding and implementing the library I used (pptxgenjs)
+      took most of the time I had.
+    </p>
+  </div>
+</main>
